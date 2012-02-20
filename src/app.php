628813c74f614d99ee8c51ab6d56f83439a904a1
Igor Wiedler <igor@wiedler.ch>
2012-02-20 20:16:38 +0100

    rm twig.path from UrlGeneratorServiceProvider

--- src/app.php
+++ src/app.php
@@ -13,9 +13,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
 
-$app->register(new Silex\Provider\UrlGeneratorServiceProvider(), array(
-    'twig.path' => __DIR__.'/../views',
-));
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
 $app['composer.doc_dir'] = __DIR__.'/../vendor/composer/composer/doc';
 
