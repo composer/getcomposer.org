53ff302f4a901645dc3c68a376c7051e924a1899
Jordi Boggiano <j.boggiano@seld.be>
2012-07-13 00:54:23 +0200

    Fix paths

--- update.sh
+++ update.sh
@@ -17,23 +17,23 @@ fi
 cd "$root/$build"
 
 # update master
-/usr/bin/git fetch -q origin && \
-/usr/bin/git fetch --tags -q origin && \
-/usr/bin/git checkout master -q && \
-/usr/bin/git rebase origin/master -q && \
+git fetch -q origin && \
+git fetch --tags -q origin && \
+git checkout master -q && \
+git rebase origin/master -q && \
 $composer install -q && \
-/usr/local/bin/php -d phar.readonly=0 $buildscript && \
+php -d phar.readonly=0 $buildscript && \
 mv $buildphar "$root/$target/$buildphar" && \
-/usr/bin/git log --pretty="%h" -n1 HEAD > "$root/$target/version"
+git log --pretty="%h" -n1 HEAD > "$root/$target/version"
 
 # create tagged releases
 for version in `git tag`; do
     if [ ! -f "$root/$target/download/$version/$buildphar" ]
     then
         mkdir -p "$root/$target/download/$version/"
-        /usr/bin/git checkout $version -q && \
+        git checkout $version -q && \
         $composer install -q && \
-        /usr/local/bin/php -d phar.readonly=0 $buildscript && \
+        php -d phar.readonly=0 $buildscript && \
         touch --date="`git log -n1 --pretty=%ci $version`" $buildphar && \
         mv $buildphar "$root/$target/download/$version/$buildphar"
     fi
