2c7ca5c8dc0f6f5b5b0fedfa6813bf5d62c6634a
Jordi Boggiano <j.boggiano@seld.be>
2014-01-21 16:59:51 +0100

    Force ssl

--- config/prod.php
+++ config/prod.php
@@ -2,6 +2,17 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+// force ssl
+$app->before(function (Request $request) {
+    // skip SSL & non-GET/HEAD requests
+    if ($request->isSecure() || !$request->isMethodSafe()) {
+        return;
+    }
+
+    $e->setResponse(new RedirectResponse('https://'.substr($request->getUri(), 7)));
+});
 
 $app->after(function (Request $request, Response $response) {
     if (!$response->headers->has('Strict-Transport-Security')) {
