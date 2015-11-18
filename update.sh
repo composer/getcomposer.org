0d407b992bee5d956d281c61285be58278e80ac0
Jordi Boggiano <j.boggiano@seld.be>
2015-11-18 18:17:01 +0000

    Only build dev phar when necessary and set the phar date correctly, fixes #79

--- update.sh
+++ update.sh
@@ -22,11 +22,18 @@ cd "$root/$build"
 git fetch -q origin && \
 git fetch --tags -q origin && \
 git checkout master -q && \
-git rebase origin/master -q && \
-$composer install -q --no-dev && \
-php -d phar.readonly=0 $buildscript && \
-mv $buildphar "$root/$target/$buildphar" && \
-git log --pretty="%H" -n1 HEAD > "$root/$target/version"
+git rebase origin/master -q
+
+version=`git log --pretty="%H" -n1 HEAD`
+
+if [ "$version" != `cat "$root/$target/$version"` ]
+then
+    $composer install -q --no-dev && \
+    php -d phar.readonly=0 $buildscript && \
+    touch --date="`git log -n1 --pretty=%ci HEAD`" "$buildphar" && \
+    mv "$buildphar" "$root/$target/$buildphar" && \
+    echo $version > "$root/$target/version"
+fi
 
 # create tagged releases
 for version in `git tag`; do
@@ -36,8 +43,8 @@ for version in `git tag`; do
         git checkout $version -q && \
         $composer install -q --no-dev && \
         php -d phar.readonly=0 $buildscript && \
-        touch --date="`git log -n1 --pretty=%ci $version`" $buildphar && \
-        mv $buildphar "$root/$target/download/$version/$buildphar"
+        touch --date="`git log -n1 --pretty=%ci $version`" "$buildphar" && \
+        mv "$buildphar" "$root/$target/download/$version/$buildphar"
         echo "$target/download/$version/$buildphar was just built and should be downloaded/committed to the repo"
     else
         touch --date="`git log -n1 --pretty=%ci $version`" "$root/$target/download/$version/$buildphar"
