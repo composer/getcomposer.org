f9c6533d5fea4d9b44c6fe12ac8ce895d45ef2b7
Jordi Boggiano <j.boggiano@seld.be>
2016-06-11 10:43:01 +0100

    Remove trimming to avoid warnings in some base64 implementations

--- bin/sign.php
+++ bin/sign.php
@@ -8,5 +8,5 @@ if (!openssl_sign(file_get_contents($_SERVER['argv'][1]), $sha384sig, $pkeyid, O
 }
 openssl_free_key($pkeyid);
 
-$sha384sig = trim(base64_encode($sha384sig), '=');
+$sha384sig = base64_encode($sha384sig);
 file_put_contents($_SERVER['argv'][1].'.sig', json_encode(['sha384' => $sha384sig], JSON_UNESCAPED_SLASHES));
