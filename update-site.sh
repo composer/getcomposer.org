6906add2dd9293f477dd45501d3efeaabdc92acb
Jordi Boggiano <j.boggiano@seld.be>
2020-02-14 14:41:43 +0100

    logic is hard

--- update-site.sh
+++ update-site.sh
@@ -3,7 +3,7 @@
 git fetch -q origin || true
 diffs=$(git diff origin/master | wc -l)
 
-if [ "0" == "$diffs" ]
+if [ "0" != "$diffs" ]
 then
     git pull -q origin master || (git clean -qfd && git pull -q origin master)
     rm -rf ./cache/twig/
