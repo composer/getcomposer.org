f990c639abb13a645256d0b4a1dfdff3e8a4a588
Jordi Boggiano <j.boggiano@seld.be>
2020-07-01 10:09:32 +0200

    Fix --2 channel to point to latest alpha for now

--- update.sh
+++ update.sh
@@ -117,10 +117,10 @@ lastVersion=$(ls "$root/$target/download" | xargs -I@ git log --format=format:"%
 lastSnapshot=$(head -c40 "$root/$target/version")
 if [ "$(ls "$root/$target/download" | grep -E '^2\.[0-9.]+$')" == "" ]
 then
-    lastStableV2Version=$lastSnapshot
-    lastStableV2VersionPath="/composer.phar"
-else
     lastStableV2Version=$(ls "$root/$target/download" | grep -E '^2\.' | xargs -I@ git log --format=format:"%ai @%n" -1 @ | sort -r | head -1 | awk '{print $4}')
+    lastStableV2VersionPath="/download/$lastStableV2Version/composer.phar"
+else
+    lastStableV2Version=$(ls "$root/$target/download" | grep -E '^2\.[0-9.]+$' | xargs -I@ git log --format=format:"%ai @%n" -1 @ | sort -r | head -1 | awk '{print $4}')
     lastStableV2VersionPath="/download/$lastStableV2Version/composer.phar"
 fi
 {
