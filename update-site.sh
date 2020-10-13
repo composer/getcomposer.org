e97007ba10e7a64e6b001f8f0d577b99d4b633dd
Jordi Boggiano <j.boggiano@seld.be>
2020-10-13 16:26:41 +0200

    Install deps, fixes #164

--- update-site.sh
+++ update-site.sh
@@ -6,5 +6,6 @@ diffs=$(git diff origin/master | wc -l)
 if [ "0" != "$diffs" ]
 then
     git pull -q origin master || (git clean -qfd && git pull -q origin master)
+    composer install -q
     rm -rf ./cache/twig/
 fi
