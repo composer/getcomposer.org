b3c61710072162971a18241e2dd2dd1e1f0e8a3c
Jordi Boggiano <j.boggiano@seld.be>
2021-02-25 10:32:48 +0100

    Render 2.0 docs until 2.1 is released

--- update-docs.sh
+++ update-docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd vendor/composer/composer
-git checkout -q master
+git checkout -q 2.0
 git fetch -q origin
-git rebase -q origin/master > /dev/null
+git rebase -q origin/2.0 > /dev/null
 cd ../../..
