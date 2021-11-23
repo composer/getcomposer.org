bb013472aad94494ecb15c2a8128d18e8bf72c52
Jordi Boggiano <j.boggiano@seld.be>
2021-11-23 14:43:10 +0100

    Update JS deps, move to algolia v3, move to plausible analytics, add frontend build system

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ change a file in the doc directory. Documentation sources can be browsed
 at https://github.com/composer/composer/tree/main/doc
 
 Documentation is then automatically generated on getcomposer.org
+
+## JS / CSS
+
+To change JS and CSS, make sure you run `npm install` first and then use
+`npm run dev` or `npm run build` to build new assets from the sources.
+
+The output of build should be committed to be deployed, but please do not commit
+it if you are sending a pull request.
---
+++ js/app.js
@@ -0,0 +1,59 @@
+import docsearch from '@docsearch/js';
+import Plausible from 'plausible-tracker'
+import '@docsearch/css';
+import 'prismjs/themes/prism.css';
+import '../css/style.css';
+import 'prismjs';
+import 'prismjs/components/prism-markup-templating';
+import 'prismjs/components/prism-php';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-json5';
+import 'prismjs/components/prism-javascript';
+
+function processQuery(query) {
+    var parts = query.split(' ');
+
+    parts = parts.map(function (part, index) {
+        var word = part.trim();
+
+        // quote --foo args so they are not parsed as negations but rather return actual results
+        if (word.length >= 2 && word[0] === '-' && word[1] === '-') {
+            word = '"' + word + (index === parts.length - 1 ? '' : '"');
+        }
+        // quote -x args
+        if (word.length == 2 && word[0] === '-') {
+            word = '"' + word + (index === parts.length - 1 ? '' : '"');
+        }
+
+        return word;
+    });
+
+    return parts.join(' ');
+}
+
+if (document.getElementById('docsearch')) {
+    docsearch({
+        container: '#docsearch',
+        appId: 'DLYN25JSFJ',
+        indexName: 'getcomposer',
+        apiKey: '59b32c5fdb7d9198939d01eebcc4ecab',
+        transformSearchClient: (searchClient) => {
+          return {
+            ...searchClient,
+            search: (spec) => {
+                let f = spec[0].query;
+                spec[0].query = processQuery(spec[0].query);
+
+                return searchClient.search(spec);
+            }
+          };
+        },
+    });
+}
+
+const plausible = Plausible({
+  domain: 'getcomposer.org',
+  apiHost: 'https://getcomposer.org',
+});
+plausible.trackPageview();
