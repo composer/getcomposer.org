66b5bbc00537a98681d27a46d86df800e700db08
Jordi Boggiano <j.boggiano@seld.be>
2024-01-26 14:49:02 +0100

    Update more deps

--- config/bundles.php
+++ config/bundles.php
@@ -4,6 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
